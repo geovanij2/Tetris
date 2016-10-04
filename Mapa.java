bce95eb819b3c3043e73bf5eaa4d7866b1bdd461
geovanij2 <geovanij3@gmail.com>
2016-10-04 11:36:33 -0300

    Add files via upload

--- Mapa.java
+++ Mapa.java
@@ -3,7 +3,7 @@ import java.util.TimerTask;
 
 public class Mapa {
 	public boolean[][] mapa = new boolean[20][10];
-	private long periodo = 200;
+	private long periodo = 500;
 	private Peca pecaAtual, pecaProxima1, pecaProxima2, pecaProxima3, pecaSegurada;
 	private boolean inicializarPecas = true;
 	private boolean podePegarProximaPeca = true;
@@ -11,12 +11,23 @@ public class Mapa {
 	private boolean alternadorDoTimer = true;
 	Timer t;
 
+	int contador = 0;
+
 	Mapa() {
 		t = new Timer();
 		TimerTask cicloDoJogo = new TimerTask() {
 			@Override
 			public void run() {
 				// tudo que estiver aqui vai ser repetido em 1 segundo
+				/*if (contador % 10 == 0) {
+					derrubaPeca();
+				}*/				
+				
+				//System.out.println(contador);
+				/*if (contador % 4 == 0){
+					seguraPeca();
+				}*/
+
 				if (alternadorDoTimer) {
 					deslocarPecaNaMatrizPrincipalVertical();
 					pegarProximaPeca();
@@ -26,6 +37,11 @@ public class Mapa {
 				} else {
 					verificaLinhasCorretas();
 					alternadorDoTimer = true;
+					if(contador%5 == 0){
+
+					girarPeca();
+					}
+						contador ++;
 				}
 			}
 
@@ -90,7 +106,7 @@ public class Mapa {
 		if (!inicializarPecas) {
 			if (verificarSePecaPodeAndarVertical()) {
 				for (int j = pecaAtual.getCoordenadaX(); j < pecaAtual.getCoordenadaX() + pecaAtual.getOrdem(); j++) {
-					// apaga a primeira linha da peça
+					// apaga a primeira linha da peca
 					atualizarOMapa(pecaAtual.getCoordenadaY(), j, false);
 				}
 				pecaAtual.descerPeca();
@@ -135,12 +151,12 @@ public class Mapa {
 			for (int i = 0; i < pecaAtual.getOrdem(); i++) {
 				for (int j = 0; j < pecaAtual.getOrdem(); j++) {
 					if (pecaAtual.getMatriz()[i][j]) {
-						atualizarOMapa(pecaAtual.getCoordenadaY() + i, pecaAtual.getCoordenadaX() + j, false);
+						atualizarOMapa(pecaAtual.getCoordenadaY() + i, pecaAtual.getCoordenadaX() + j, false); // limpa antigo local da peca no mapa
 					}
 				}
 			}
 			pecaAtual.resetarCoordenadas();
-			if (pecaSegurada == null) {
+			if (pecaSegurada == null) { // checa se ja tem peca segurada
 				pecaSegurada = pecaAtual;
 				podePegarProximaPeca = true;
 				pegarProximaPeca();
@@ -205,5 +221,48 @@ public class Mapa {
 			}
 		}
 		excluirLinha(0);
+	}
+
+	public void derrubaPeca() {
+		 for (int i = 0; i < 20; i++) {
+		 	deslocarPecaNaMatrizPrincipalVertical();
+		 }
+	}
+	public void girarPeca() {
+		if(verificaGiro()) {	
+			for (int i = 0; i < pecaAtual.getOrdem(); i++){
+				for(int j = 0; j < pecaAtual.getOrdem(); j++){
+					if (pecaAtual.getMatriz()[i][j]) {
+						atualizarOMapa(pecaAtual.getCoordenadaY() + i, pecaAtual.getCoordenadaX() + j, false);
+					}
+				}
+			}
+			pecaAtual.girar();
+		}	
+	}
+	public boolean verificaGiro(){ // verifica se a peca pode girar
+		boolean resposta = true;
+		boolean[][] teste = pecaAtual.getMatriz();
+
+		int ordem = teste.length;//gira a peca p direita
+		boolean[][] b = new boolean[ordem][ordem];
+		for(int i = 0; i < teste.length; i++){
+			for(int j = 0; j < teste.length; j++){
+				b[j][ordem-1] = teste[i][j];
+			}
+			ordem--;
+		}
+		teste = b;
+
+		for (int i = 0; i < pecaAtual.getOrdem(); i ++){
+			for (int j = 0; j < pecaAtual.getOrdem(); j++) {
+				if(!(teste[i][j] && pecaAtual.getMatriz()[i][j])){
+					if(teste[i][j] && mapa[pecaAtual.getCoordenadaY()+i][pecaAtual.getCoordenadaX()+j]){
+						resposta = false;
+					}
+				}
+			}
+		}
+		return resposta;
 	}
 }
